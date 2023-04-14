2f8f7c35e960f3d1ff295fa7a04c9dbee703e161
AzureMichael <mihailovici111@gmail.com>
2023-04-14 11:27:18 +0300

    Initial project setup

---
+++ ttrpg-character-manager/src/main/java/com/digitaldg/ttrpgcharactermanager/TtrpgCharacterManagerApplication.java
@@ -0,0 +1,13 @@
+package com.digitaldg.ttrpgcharactermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtrpgCharacterManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtrpgCharacterManagerApplication.class, args);
+	}
+
+}
---
+++ ttrpg-character-manager/src/test/java/com/digitaldg/ttrpgcharactermanager/TtrpgCharacterManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitaldg.ttrpgcharactermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtrpgCharacterManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
